0ac67db89baf44b1f5cf794dfc5d5b027e5709e2
karimazizc <karimazizc@gmail.com>
2026-02-04 21:54:19 +0700

    added shopee floating button

--- home/models.py
+++ home/models.py
@@ -648,6 +648,24 @@ class HomePage(AbstractEmailForm):
         default="Hi, I would like to get a quote for air conditioning service",
         help_text="Default WhatsApp message"
     )
+    
+    # Shopee Floating Button Settings
+    shopee_button_enabled = models.BooleanField(
+        default=False,
+        help_text="Enable/disable the Shopee floating button"
+    )
+    shopee_url = models.URLField(
+        blank=True,
+        default="https://shopee.com.my/",
+        help_text="URL to your Shopee store"
+    )
+    shopee_button_text = models.CharField(
+        max_length=50,
+        blank=True,
+        default="",
+        help_text="Optional text to display next to the Shopee icon (leave blank for icon only)"
+    )
+    
     google_maps_embed = models.TextField(
         default='<iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=31,%20Jalan%20Bendahara%202,%20taman%20sejati,%2041200%20Klang,%20Selangor+(Seng%20Leong%20Engineering%20Sdn%20Bhd)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>',
         help_text="Google Maps embed code"
@@ -825,6 +843,11 @@ class HomePage(AbstractEmailForm):
             FieldPanel('whatsapp_number'),
             FieldPanel('whatsapp_message'),
         ], heading="Social Media"),
+        MultiFieldPanel([
+            FieldPanel('shopee_button_enabled'),
+            FieldPanel('shopee_url'),
+            FieldPanel('shopee_button_text'),
+        ], heading="Shopee Floating Button"),
         FieldPanel('google_maps_embed'),
     ]
     
